58c6cc1b7609b5652b45fa391affe803092487e6
soloslee <ssoloim@gmail.com>
2017-05-18 21:59:57 +0800

    use switch true to replace long if-then-else chains

--- src/JsonResponse.php
+++ src/JsonResponse.php
@@ -6,55 +6,58 @@ use Response;
 
 class JsonResponse
 {
-    const ERROR = 'error';
-    const SUCCESS = 'success';
+    const ERR = 'error';
+    const SUCC = 'success';
 
     private function toString($data)
     {
-        if (is_bool($data)) {
-            return $data ? '1' : '0';
-        } elseif (is_integer($data) || is_double($data)) {
-            return $data . '';
-        } elseif (is_string($data)) {
-            return $data;
-        } elseif (is_null($data)) {
-            return null;
-        } elseif (is_object($data)) {
-            if (is_callable(array($data, 'toArray'))) {
-                return $this->toString($data->toArray());
-            } elseif (is_callable(array($data, 'getAttributes'))) {
-                return $this->toString($data->getAttributes());
-            } elseif (is_callable(array($data, '__toString'))) {
-                return $data->__toString();
-            } elseif (get_class($data) === 'stdClass' && $data === new \stdClass()) {
+        switch (gettype($data)) {
+            case 'boolean':
+                return $data ? '1' : '0';
+            case 'integer':
+            case 'double':
+                return $data . '';
+            case 'string':
+                return $data;
+            case 'NULL':
                 return null;
-            } else {
-                return $this->toString((array) $data);
-            }
-        } elseif (is_array($data)) {
-            $output = array();
+            case 'object':
+                switch (true) {
+                    case is_callable(array($data, 'toArray')):
+                        return $this->toString($data->toArray());
+                    case is_callable(array($data, 'getAttributes')):
+                        return $this->toString($data->getAttributes());
+                    case is_callable(array($data, '__toString')):
+                        return $data->__toString();
+                    case (get_class($data) === 'stdClass' && $data === new \stdClass()):
+                        return null;
+                    default:
+                        return $this->toString((array) $data);
+                }
+            case 'array':
+                $output = array();
 
-            foreach ($data as $key => $value) {
-                $output[$key] = $this->toString($value);
-            }
+                foreach ($data as $key => $value) {
+                    $output[$key] = $this->toString($value);
+                }
 
-            return $output;
-        } else {
-            return (string) $data;
+                return $output;
+            default:
+                return (string) $data;
         }
     }
 
-    public function toArray($data, $status, $errorMessage = null, $errorCode = null)
+    public function toArray($data, $status, $errMsg = null, $errCode = null)
     {
         $arr = array('status' => $status);
 
-        if ($status === self::SUCCESS && $data) {
+        if ($status === self::SUCC && $data) {
             $arr['data'] = $data;
         } elseif ($status === self::ERROR) {
-            $arr['message'] = (string) $errorMessage;
+            $arr['message'] = (string) $errMsg;
 
-            if ($errorCode) {
-                $arr['code'] = $errorCode;
+            if ($errCode) {
+                $arr['code'] = $errCode;
             }
         }
 
@@ -64,15 +67,15 @@ class JsonResponse
     public function success($data = null)
     {
         $output = $this->toString($data);
-        $success = $this->toArray($output, self::SUCCESS);
+        $succ = $this->toArray($output, self::SUCC);
 
-        return Response::json($success, 200);
+        return Response::json($succ, 200);
     }
 
-    public function error($errorMessage = null, $errorCode = null)
+    public function error($errMsg = null, $errCode = null)
     {
-        $error = $this->toArray(null, self::ERROR, $errorMessage, $errorCode);
+        $err = $this->toArray(null, self::ERR, $errMsg, $errCode);
 
-        return Response::json($error, 202);
+        return Response::json($err, 202);
     }
 }
